e6625cd4c70fec65f7a759920ac9c4e3d9523e13
DOÃN PHAN NGUYỄN KHẮC <1612108@student.hcmus.edu.vn>
2019-06-18 13:10:00 +0700

    fix postmodel

--- models/post.model.js
+++ models/post.model.js
@@ -13,8 +13,12 @@ module.exports = {
         return db.load(`select * from posts where category_id = ${idCate} order by posts.publish_date DESC`);
     },
 
-    allByCate: (idCate, offset, limit) => {
-        return db.load(`select p.*, c.name as category_name from(select * from posts where category_id = ${idCate} limit ${offset},${limit}) p, category c where p.category_id = c.id`);
+
+    allByCate: (idCate, offset, limit, ispremium) => {
+        var sql = `select p.*, c.name as category_name from(select * from posts where category_id = ${idCate}`;
+        sql += ispremium == false ? `and ispremium=0` : ``;
+        sql += `limit ${offset},${limit}) p, category c where p.category_id = c.id`;
+        return db.load(sql);
     },
 
     postLimit: (offset, limit) => {
@@ -25,8 +29,11 @@ module.exports = {
         return db.load(`SELECT p.*, cate.name as topic, user.name as author FROM posts p JOIN category cate ON p.category_id = cate.id JOIN user ON p.user_id = user.id WHERE p.id != ${id} and cate.id = ${idCate} order by p.publish_date DESC LIMIT 5`);
     },
 
-    postLimitWittCategoryName: (offset, limit) => {
-        return db.load(`select p.* , c.name as category_name from (select * from posts order by posts.publish_date desc limit ${offset},${limit}) p, category c where p.category_id = c.id`)
+    postLimitWittCategoryName: (offset, limit, ispremium) => {
+        var sql = `select p.* , c.name as category_name from (SELECT * FROM posts where ispremium=0 ORDER BY posts.publish_date desc limit ${offset},${limit}) p, category c where p.category_id = c.id`;
+        if (ispremium == true)
+            sql = `select p.* , c.name as category_name from (SELECT * FROM posts ORDER BY posts.ispremium desc, posts.publish_date desc limit ${offset},${limit}) p, category c where p.category_id = c.id`;
+        return db.load(sql);
     },
 
 
@@ -42,8 +49,21 @@ module.exports = {
         return db.load(`select DISTINCT cg.name as cg_name, c.name as c_name from categorygroup cg, category c, posts p WHERE c.categorygroup_id = cg.id and c.id = ${cateId}`);
     },
 
-    allByTag: (tagId, offset, limit) => {
-        return db.load(`select p.*, t.id as tag_id, t.name as tag_name from posts p, tag t, post_tag WHERE p.id = post_tag.post_id and t.id = post_tag.tag_id and post_tag.tag_id = ${tagId} limit ${offset},${limit}`);
+    allByTag: (tagId, offset, limit, ispremium) => {
+        var sql = `select p.*, t.id as tag_id, t.name as tag_name from posts p, tag t, post_tag WHERE p.id = post_tag.post_id and t.id = post_tag.tag_id and post_tag.tag_id = ${tagId} `;
+        sql+= ispremium==false? ` and ispremium=0 `:``;
+        sql+=`ORDER BY p.ispremium desc, p.publish_date desc limit ${offset},${limit}`;
+        console.log(sql);
+        return db.load(sql);
+    },
+
+    countPostByCate: (ispremium , cateID = null) => {
+        var sql = `select count(*) as numb_of_posts from posts where `;
+        sql += cateID != null ? `posts.category_id = ${cateID} ` : `1 `;
+        sql += ispremium == false ? `and ispremium=0` : ``;
+        console.log(sql);
+        console.log(sql);
+        return db.load(sql);
     },
 
     countPostByCate: (cateID = null) => {
@@ -86,9 +106,9 @@ module.exports = {
         return db.delete('posts', 'id', id);
     },
 
-    allByUserIdAndStatus: (user_id, status=null) => {
+    allByUserIdAndStatus: (user_id, status = null) => {
         var sql = `select p.*, c.name as category_name from (SELECT * from posts where user_id = ${user_id}) p, category c where p.category_id = c.id`;
-        sql += status!=null? ` and status = ${status}`:``;
+        sql += status != null ? ` and status = ${status}` : ``;
         return db.load(sql);
     },
     // search
